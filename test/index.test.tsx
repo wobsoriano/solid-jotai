249bfc5bf818372fd369ca5afda068e5418542be
wobsoriano <sorianorobertc@gmail.com>
2023-01-16 19:53:19 -0800

    update test

--- test/index.test.tsx
+++ test/index.test.tsx
@@ -5,9 +5,11 @@ import { atom, useAtom } from '../src'
 test('should function correct when rendering in solid', () => {
   const div = document.createElement('div')
   const countAtom = atom(0)
+  const doubledCountAtom = atom(get => get(countAtom) * 2)
 
   render(() => {
     const [count, setCount] = useAtom(countAtom)
+    const [doubled] = useAtom(doubledCountAtom)
 
     function increase() {
       setCount(prev => prev + 1)
@@ -17,7 +19,7 @@ test('should function correct when rendering in solid', () => {
     increase()
     increase()
     increase()
-    return <span>{count()}</span>
+    return <span>{count()} - {doubled()}</span>
   }, div)
-  expect(div.innerHTML).toBe('<span>3</span>')
+  expect(div.innerHTML).toBe('<span>3 - 6</span>')
 })
