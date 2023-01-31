4cee0f70720b00b8b51740113115e368a9f94532
wobsoriano <sorianorobertc@gmail.com>
2023-01-31 08:22:37 -0800

    feat(deps): add useHydrateAtoms util

---
+++ src/utils/useHydrateAtoms.ts
@@ -0,0 +1,32 @@
+import { useStore } from '../Provider'
+import type { WritableAtom } from 'jotai/vanilla'
+
+type Store = ReturnType<typeof useStore>
+type Options = Parameters<typeof useStore>[0]
+type AnyWritableAtom = WritableAtom<unknown, any[], any>
+
+const hydratedMap: WeakMap<Store, WeakSet<AnyWritableAtom>> = new WeakMap()
+
+export function useHydrateAtoms(
+  values: Iterable<readonly [AnyWritableAtom, unknown]>,
+  options?: Options
+) {
+  const store = useStore(options)
+
+  const hydratedSet = getHydratedSet(store)
+  for (const [atom, value] of values) {
+    if (!hydratedSet.has(atom)) {
+      hydratedSet.add(atom)
+      store.set(atom, value)
+    }
+  }
+}
+
+const getHydratedSet = (store: Store) => {
+  let hydratedSet = hydratedMap.get(store)
+  if (!hydratedSet) {
+    hydratedSet = new WeakSet()
+    hydratedMap.set(store, hydratedSet)
+  }
+  return hydratedSet
+}
